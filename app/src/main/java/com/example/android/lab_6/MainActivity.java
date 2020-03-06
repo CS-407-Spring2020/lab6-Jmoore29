aa99ea63aff5cb1d3a1b173e9a640e61178eb6c0
Jmoore29 <moore29@wisc.edu>
2020-03-06 14:25:32 -0600

    milestone 2

--- app/src/main/java/com/example/android/lab_6/MainActivity.java
+++ app/src/main/java/com/example/android/lab_6/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.android.lab_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.security.acl.LastOwnerException;
 
 public class MainActivity extends AppCompatActivity {
 
-    //SomeWhere in Australia
-    private final LatLng mDestinationLatLng = new LatLng(43.0752817, -89.4063554);
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private final LatLng mDestinationLatLng = new LatLng(43.075411, -89.404220);
     private GoogleMap mMap;
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +36,49 @@ public class MainActivity extends AppCompatActivity {
         mapFragment.getMapAsync(googleMap -> {
             mMap = googleMap;
             googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Destination"));
+            displayMyLocation();
         });
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void displayMyLocation(){
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if(permission == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        else{
+            mFusedLocationProviderClient.getLastLocation().addOnCompleteListener(this, task -> {
+                Location mLastKnownLocation = task.getResult();
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+                mapFragment.getMapAsync(googleMap -> {
+                    mMap = googleMap;
+                    googleMap.addMarker(new MarkerOptions().position(new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude())).title("Current"));
+                });
+                if(task.isSuccessful() && mLastKnownLocation != null){
+                    mMap.addPolyline(new PolylineOptions().add(new LatLng(mLastKnownLocation.getLatitude(),
+                            mLastKnownLocation.getLongitude()), mDestinationLatLng));
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION){
+            // If request cancelled, the result arrays are empty
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                displayMyLocation();
+            }
+        }
 
     }
+
+
+
+
+
+
 }
